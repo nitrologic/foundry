1697302c492c261c23b4e58e9ebe266ea06b248e
simon m2 <nitrologic@gmail.com>
2025-04-17 14:45:42 +1200

    test2 nearing it

--- isolation/test2.js
+++ isolation/test2.js
@@ -38,3 +38,69 @@ for(let i=0;i<5;i++){
     host.port.postMessage({name:"ping",i});
     sleep(1e3);
 }
+
+
+
+
+
+
+//async function promptFoundry(message) {
+//		await writer.write(encoder.encode(message));
+//		await writer.ready;
+//	}
+
+
+async function exitTest(){
+
+}
+async function radar(){
+	var promptBuffer = new Uint8Array(0);
+	Deno.stdin.setRaw(true);
+	try {
+		let busy = true;
+		while (busy) {
+			const { value, done } = await reader.read();
+			if (done || !value) break;
+			var bytes = [];
+			for (const byte of value) {
+				if (byte === 0x7F || byte === 0x08) { // Backspace
+					if (promptBuffer.length > 0) {
+						promptBuffer = promptBuffer.slice(0, -1);
+						bytes.push(0x08, 0x20, 0x08);
+					}
+				} else if (byte === 0x1b) { // Escape sequence
+					if (value.length === 1) {
+						await exitTest();
+						Deno.exit(0);
+					}
+					if (value.length === 3) {
+						if (value[1] === 0xf4 && value[2] === 0x50) {
+							echo("F1");
+						}
+					}
+					break;
+				} else if (byte === 0x0A || byte === 0x0D) { // Enter key
+					bytes.push(0x0D, 0x0A);
+					let line = decoder.decode(promptBuffer);
+					let n = line.length;
+					if (n > 0) {
+						promptBuffer = promptBuffer.slice(n);
+					}
+					result = line.trimEnd();
+					log(result, "stdin");
+					busy = false;
+				} else {
+					bytes.push(byte);
+					const buf = new Uint8Array(promptBuffer.length + 1);
+					buf.set(promptBuffer);
+					buf[promptBuffer.length] = byte;
+					promptBuffer = buf;
+				}
+			}
+			if (bytes.length) await writer.write(new Uint8Array(bytes));
+		}
+	} finally {
+		Deno.stdin.setRaw(false);
+	}
+	return result;
+}
